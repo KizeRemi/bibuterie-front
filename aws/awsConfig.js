a1bd367e3022e7fcdf17f9e9e6000f406af29f67
Rémi Mavillaz <remimavillaz@macbook-pro-de-remi-2.home>
2020-06-09 21:50:12 +0200

    add env

--- aws/awsConfig.js
+++ aws/awsConfig.js
@@ -3,12 +3,12 @@ export default {
     identityPoolId: 'eu-west-1:f44e1031-2b65-4e74-8d76-be405dfb38e8',
     identityPoolRegion: 'eu-west-1',
     userPoolId: 'eu-west-1_K2m1icWIj',
-    userPoolWebClientId: '3pr6fdi8g3domth9huceohra3q',
+    userPoolWebClientId: process.env.NODE_ENV === 'production' ? '7db59u7houaifcfgq2gi19s656' : '3pr6fdi8g3domth9huceohra3q',
     oauth: {
         domain: 'bibuterie.auth.eu-west-1.amazoncognito.com',
         scope: ['email', 'profile', 'openid'],
-        redirectSignIn: process.env.NODE_ENV === 'production' ? 'https://bibuterie-front.herokuapp.com' : 'http://localhost:3000/',
-        redirectSignOut:  process.env.NODE_ENV === 'production' ? 'https://bibuterie-front.herokuapp.com' : 'http://localhost:3000/',
+        redirectSignIn: process.env.NODE_ENV === 'production' ? 'https://bibuterie-front.herokuapp.com/' : 'http://localhost:3000/',
+        redirectSignOut:  process.env.NODE_ENV === 'production' ? 'https://bibuterie-front.herokuapp.com/' : 'http://localhost:3000/',
         responseType: 'code'
     }
 }

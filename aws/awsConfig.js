bcfe12b4e34be8162d2b1fab0caaba9c2c9c0db2
Rémi Mavillaz <remimavillaz@macbook-pro-de-remi-2.home>
2020-06-12 13:45:32 +0200

    improve design and walking page

--- aws/awsConfig.js
+++ aws/awsConfig.js
@@ -1,6 +1,5 @@
 export default {
     region: 'eu-west-1',
-    identityPoolId: 'eu-west-1:f44e1031-2b65-4e74-8d76-be405dfb38e8',
     identityPoolRegion: 'eu-west-1',
     userPoolId: 'eu-west-1_K2m1icWIj',
     userPoolWebClientId: process.env.NODE_ENV === 'production' ? '66tbtonbvlpu76vt4bv573f59f' : '3pr6fdi8g3domth9huceohra3q',
